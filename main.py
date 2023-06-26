e877c2d33ebc794caf0105c50affeba5b8f582d0
Marcin C <81714575+chinek01@users.noreply.github.com>
2023-06-26 22:31:44 +0200

    init file

---
+++ main.py
@@ -0,0 +1,57 @@
+"""
+
+Portfolio 01
+Morse Code Converter
+#100DaysOfCode with Python
+Day: 81
+Date: 2023-06-16
+Author: MC
+
+"""
+
+MORSE_CODE_DIC = {
+    'A': '.-.',
+    'B': '-...',
+    'C': '-.-.',
+    'D': '-..',
+    'E': '.',
+    'F': '..-.',
+    'G': '--.',
+    'H': '....',
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.',
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--',
+    'Z': '--..',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    '0': '-----',
+}
+
+word = input("Enter a word:  ").upper()
+
+output_list = [MORSE_CODE_DIC[letter] for letter in word]
+output = ' '.join(output_list)
+
+# print(output_list)
+print(output)
